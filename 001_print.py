6cbdfe597ebb61e0c234bba14e4fa7e11fa458c0
Rizwan Khan <i.rizzwankhan@gmail.com>
2025-11-13 18:38:32 +0530

    print() function

---
+++ 001_print.py
@@ -0,0 +1,5 @@
+# the print() function in Python is used to output text or other data to the console.
+
+print("Hello, World!")
+print("This is a simple print statement.")
+print("You can print numbers too:", 42)
